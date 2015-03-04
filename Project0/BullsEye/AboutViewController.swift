bf40dcbb9e8b0026facf165701a895b0d6897ec8
T. Bedford <tbedford@bo109-lab-5040.labs.msu>
2015-03-04 16:16:59 -0600

    Commit Project 0

---
+++ Project0/BullsEye/AboutViewController.swift
@@ -0,0 +1,14 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by T. Bedford on 3/2/15.
+//  Copyright (c) 2015 Teon Bedford. All rights reserved.
+//
+
+import UIKit
+class AboutViewController: UIViewController {
+    @IBAction func close() {
+    dismissViewControllerAnimated(true, completion: nil)
+    }
+}
